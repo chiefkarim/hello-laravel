d8f69da28dd8b1038c40bc3188d8eb532be256be
chiefkarim <krimotop2016@gmail.com>
2025-05-17 21:03:40 +0100

    refactor: use eloquent to get jobs form database

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,27 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            ['id' => 1, 'title' => 'teacher', 'salary' => '50k'],
-            ['id' => 2, 'title' => 'plumber', 'salary' => '60k'],
-            ['id' => 3, 'title' => 'engineer', 'salary' => '10k'],
-        ];
-    }
-
-    public static function findById(int $id): array
-    {
-        $job = Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $table = 'job_listings';
 }
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => Job::all()]);
+    $jobs = Job::all();
+
+    return view('jobs', ['jobs' => $jobs]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $job = Job::findById($id);
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
